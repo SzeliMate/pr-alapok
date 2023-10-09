efe7eaffa89d5dd03718db96492743f2b70c7b52
Szeli <112968347+SzeliMate@users.noreply.github.com>
2023-10-09 14:15:02 +0200

    Add files via upload

---
+++ 10-11-ora/szamonkeres_01.py
@@ -0,0 +1,20 @@
+print("Szeli Máté 10/B")
+jegy = int(input("Kérek egy jegyet 1-5: "))
+
+if jegy == 5:
+    print("A jegyed 5: Jeles!")
+
+if jegy == 4:
+    print("A jegyed 4: Jó!")
+
+if jegy == 3:
+    print("A jegyed 3: Közepes!")
+
+if jegy == 2:
+    print("A jeyged 2: Elégséges!")
+
+if jegy == 1:
+    print("A jegyed 1: Elégtelen!")
+
+elif jegy ==6:
+    print("A jegyed 6: Nem megfelelő!")
